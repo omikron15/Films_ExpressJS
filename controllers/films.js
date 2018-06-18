32b3869af88ef093615977a5665cd17dafce6116
omikron15 <omikron15@hotmail.co.uk>
2018-06-18 20:57:45 +0100

    routes completed for film controller

--- controllers/films.js
+++ controllers/films.js
@@ -6,7 +6,8 @@ const express = require('express');
 const filmRouter = new express.Router();
 
 filmRouter.get("/:id", function(req, res){
-
+  const index = req.params.id;
+  res.json({data: films[index]});
 })
 
 filmRouter.get("/", function(req, res){
@@ -14,15 +15,28 @@ filmRouter.get("/", function(req, res){
 })
 
 filmRouter.put("/:id", function(req, res){
+  const index = req.params.id;
+  films[index] = req.body;
+  res.json({data: films});
+})
 
+filmRouter.post("/:id/review", function(req, res){
+  const newReiew = new Review(req.body);
+  const index = req.params.id;
+  films[index].addReview(newReiew)
+  res.json({data: films})
 })
 
 filmRouter.post("/", function(req, res){
-
+  const newFilm = new Film(req.body);
+  films.push(newFilm);
+  res.json({data: films});
 })
 
 filmRouter.delete("/:id", function(req, res){
-
+  const index = req.params.id;
+  films.splice(index, 1);
+  res.json({data: films});
 })
 
 module.exports = filmRouter;
