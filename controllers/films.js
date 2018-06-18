62a0f65466692fe0ea59e0381959ec9908ac9fea
omikron15 <omikron15@hotmail.co.uk>
2018-06-18 17:02:01 +0100

    adds films controller with templates for all routes. All films route is working correctly

--- controllers/films.js
+++ controllers/films.js
@@ -1,3 +1,28 @@
 const films = require('../models/films')();
 const Film = require('../models/film');
 const Review = require('../models/review');
+
+const express = require('express');
+const filmRouter = new express.Router();
+
+filmRouter.get("/:id", function(req, res){
+
+})
+
+filmRouter.get("/", function(req, res){
+  res.json({data: films});
+})
+
+filmRouter.put("/:id", function(req, res){
+
+})
+
+filmRouter.post("/", function(req, res){
+
+})
+
+filmRouter.delete("/:id", function(req, res){
+
+})
+
+module.exports = filmRouter;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = new express.Router();
 
+
+router.use("/films", require("./films.js"));
+
 router.get('/', function(req, res) {
   res.json({ data: 'Sup!'});
 });
